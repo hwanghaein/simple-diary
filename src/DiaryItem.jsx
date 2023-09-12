941157199a6891301d86a2cf4ffbd9ca7b7c03c2
hwanghaein <hhi9037@naver.com>
2023-09-12 22:51:24 +0900

    데이터 수정, 삭제

--- src/DiaryItem.jsx
+++ src/DiaryItem.jsx
@@ -1,4 +1,47 @@
-const DiaryItem = ({author, content, created_date, emotion, id}) => {
+import { useRef } from "react";
+import { useState } from "react";
+
+const DiaryItem = ({
+  onEdit,
+  onDelete,
+  author,
+  content,
+  created_date,
+  emotion,
+  id,
+}) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const toggleIsEdit = () => setIsEdit(!isEdit);
+
+  const [localContent, setLocalContent] = useState(content);
+  const localContentInput =  useRef(); // 레퍼런스 객체 만들기
+
+  const handleDelete = () => {
+    console.log(id);
+    if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+      onDelete(id);
+    }
+  };
+
+const handleQuitEdit = () => { 
+  setIsEdit(false);
+  setLocalContent(content);
+}
+
+const handleEdit = () =>{
+  if(localContent.length < 5){
+    localContentInput.current.focus(); // 포커스 명령 넣기
+    return;
+  }
+
+  if(window.confirm(`${id}번 째 일기를 수정하시겠습니까?`)){
+    onEdit(id, localContent);
+    toggleIsEdit();
+  }
+
+
+}
+
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -8,7 +51,31 @@ const DiaryItem = ({author, content, created_date, emotion, id}) => {
         <br />
         <span className="date">{new Date(created_date).toLocaleString()}</span>
       </div>
-      <div className="content">{content}</div>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput} // textarea에 레퍼런스로 추가 
+              value={localContent}
+              onChange={(e) => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
+
+      {isEdit ? (
+        <>
+          <button onClick={handleQuitEdit}>수정 취소</button>
+          <button onClick={handleEdit}>수정 완료</button>
+        </>
+      ) : (
+        <>
+          <button onClick={handleDelete}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
+      )}
     </div>
   );
 };
--- src/DiaryList.jsx
+++ src/DiaryList.jsx
@@ -1,14 +1,14 @@
 import DiaryItem from './DiaryItem';
 
-const DiaryList = ({diaryList})=> {
+const DiaryList = ({ onEdit, onDelete,diaryList})=> {
   return (
   <div className="DiaryList">
     <h2>일기 리스트</h2>
     <h4>{diaryList.length}개의 일기가 있습니다.</h4>
   <div>
     {diaryList.map((it)=>
-    (   // unique "key" prop 에러 해결 (최상위 태그에 적기)
-      <DiaryItem key={it.id} {...it}/>
+    ( 
+      <DiaryItem key={it.id} {...it} onEdit={onEdit} onDelete={onDelete}/>
 
   ))}
   </div>
@@ -17,9 +17,9 @@ const DiaryList = ({diaryList})=> {
   );
 }
 
-// undefined로 전달 될 경우를 대비해서 props의 기본값을 설정해줌
+
 DiaryList.defaultProps={
-diaryList:[] // 빈배열로 기본값 설정
+diaryList:[] 
 }
 
 export default DiaryList;
