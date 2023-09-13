8fa57bf8e7e249ab6da9f1b4afb9ef865bacfa6e
hwanghaein <hhi9037@naver.com>
2023-09-13 21:12:49 +0900

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
 import DiaryEditor from "./DiaryEditor/DiaryEditor";
 import DiaryList from "./DiaryList";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
+import OptimizeTest from "./OptimizeTest";
 
-// https://jsonplaceholder.typicode.com/comments
 
 function App() {
   const [data, setData] = useState([]);
@@ -45,7 +43,6 @@ function App() {
   };
 
   const onDelete = (targetId) => {
-    console.log(`${targetId}가 삭제되었습니다.`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
   };
@@ -58,9 +55,27 @@ function App() {
     );
   };
 
+  const getDiaryAnalysis = useMemo(
+    () => {
+    
+    const goodCount = data.filter((it)=>it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return {goodCount,badCount, goodRatio};
+  }, [data.length]
+  );
+
+  const {goodCount,badCount, goodRatio} = getDiaryAnalysis;
+
   return (
     <div className="App">
+      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수: {goodCount}</div>
+      <div>기분 나쁜 일기 개수: {badCount}</div>
+      <div>기분 좋은 일기 비율: {goodRatio}</div>
+
       <DiaryList onEdit={onEdit} onDelete={onDelete} diaryList={data} />
     </div>
   );
